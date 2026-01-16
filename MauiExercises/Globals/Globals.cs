eef309bff3d95a46472dd4af8ff1dbcdc2fae9e3
Martin <martin_lenart@icloud.com>
2026-01-16 13:20:46 +0100

    update

--- MauiExercises/Globals/Globals.cs
+++ MauiExercises/Globals/Globals.cs
@@ -5,21 +5,24 @@ namespace MauiExercises.Globals
     public class Global
 
     {
-        #region Lazy implementation
+        #region singleton implementation
+        private static Global _instance = null;
         Global() { } // just to avoid any direct instantiations
-
-        private static Lazy<Global> _instance = new Lazy<Global>(() => new Global());
-        public static Global Data => _instance.Value;
+        public static Global Data => (_instance == null) ? _instance = new Global() : _instance;
         #endregion
 
         #region Data implementation 
-        // accessible as GlobalData.Data.xx 
+        // accessible as Global.Message and Global.Time 
         public string Message { get; set; }
         public DateTime Time { get; set; }
+        #endregion
+
+        #region Lazy Data implementation 
 
         //Exercise:
         // Add properties so Global.Data contain a list of Friend,
         // a list of City, and a list on NamedColor
+
         #endregion
     }
 }
--- MauiExercises/Views/Lesson04/StateManagement1.xaml.cs
+++ MauiExercises/Views/Lesson04/StateManagement1.xaml.cs
@@ -9,14 +9,17 @@ namespace MauiExercises.Views.Lesson04
 {
     public partial class StateManagement1 : ContentPage
     {
+        public List<Friend> MyFriends;
+        public List<City> MyCities;
+        public List<NamedColor> MyColor;
         public StateManagement1()
         {
             InitializeComponent();
+            BindingContext = this;
 
             var startMessage = Global.Data.Message;
             var startTime = Global.Data.Time;
 
-            BindingContext= this;
         }
     }
 }
